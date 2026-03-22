c2d9d86deb0b14190d5b8cf0de58baf19e6415d2
Danilo <danilo.merlo@sou.inteli.edu.br>
2026-03-22 20:38:39 -0300

    feat: add K6 load test

---
+++ load_test/k6_load_test.js
@@ -0,0 +1,25 @@
+import http from 'k6/http'
+import { check, sleep } from 'k6'
+
+export const options = {
+  vus: 100000,
+  duration: '30s'
+}
+
+export default function () {
+  const data = {
+    device_id:    "device-01",
+    timestamp:    "2024-01-01T00:00:00Z",
+    sensor_type:  "temperatura",
+    reading_type: "analogica",
+    value:        "23.5",
+  }
+
+  const params = {
+    headers: { 'Content-Type': 'application/json' },
+  }
+
+  let res = http.post('http://localhost:8088/dados', JSON.stringify(data), params)
+  check(res, { 'status 200': (r) => r.status === 200 })
+  sleep(0.3)
+}
